ff3c0fc809302489f0748527a299f827469d9ea0
lcian <17258265+lcian@users.noreply.github.com>
2025-10-18 10:43:35 +0200

    ref(keymaps): restructure

--- lua/flowistry/keymaps.lua
+++ lua/flowistry/keymaps.lua
@@ -5,25 +5,47 @@ local logger = require("flowistry.logger")
 ---@return flowistry.keymaps
 local M = {}
 
+local keymaps = {
+  {
+    map = "<leader>nf",
+    cmd = "<cmd>Flowistry focus toggle<cr>",
+    desc = "Toggle focus",
+    mode = "n",
+  },
+  {
+    map = "<leader>nm",
+    cmd = "<cmd>Flowistry mark set<cr>",
+    desc = "Set mark",
+    mode = "n",
+  },
+  {
+    map = "<leader>nr",
+    cmd = "<cmd>Flowistry mark remove<cr>",
+    desc = "Remove mark",
+    mode = "n",
+  },
+}
+
 ---Sets up the default keymaps
-M.setup = function()
-  logger.info("Setting up default keymaps")
+function M.setup()
+  logger.debug("flowistry.keymaps.setup()")
 
-  local wk_ok, wk = pcall(require, "which-key")
-  if wk_ok then
-    logger.info("Setting up default keymaps using which-key")
-    wk.add({
+  local ok, wk = pcall(require, "which-key")
+  if ok then
+    logger.debug("using which-key")
+    local wk_keymaps = {
       { "<leader>n", group = "Flowistry" },
-      { "<leader>nf", "<cmd>Flowistry focus toggle<cr>", desc = "Toggle focus", mode = "n" },
-      { "<leader>nm", "<cmd>Flowistry mark set<cr>", desc = "Set mark", mode = "n" },
-      { "<leader>nr", "<cmd>Flowistry mark remove<cr>", desc = "Remove mark", mode = "n" },
-    })
+    }
+    for _, keymap in ipairs(keymaps) do
+      table.insert(wk_keymaps, { keymap.map, keymap.cmd, desc = keymap.desc, mode = keymap.mode })
+    end
+    wk.add(wk_keymaps)
   else
-    logger.info("Setting up default keymaps using vim api")
-    logger.error("Not implemented")
+    logger.debug("using vim api")
+    for _, keymap in ipairs(keymaps) do
+      vim.keymap.set(keymap.mode, keymap.map, keymap.cmd, { desc = keymap.desc })
+    end
   end
-
-  logger.info("Set up default keymaps")
 end
 
 return M
